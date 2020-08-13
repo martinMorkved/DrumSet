593955f22f3eba0ee775900f71390ec4623c33b7
martinMorkved <38659462+martinMorkved@users.noreply.github.com>
2020-08-13 07:44:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        var audio = new Audio("sounds/tom-1.mp3");
-        audio.play();
-
-        //What to do when click detected.
+        this.style.color = "white";
     });
 }
+
+//var audio = new Audio("sounds/tom-1.mp3");
+//      audio.play();
